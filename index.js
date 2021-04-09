2c7801e7a5ef0dc64f0e43347456d6c83928834c
emeltec <emelgarejox@gmail.com>
2021-04-09 02:39:11 -0500

    Webapp con nodejs y ejs

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const ejs = require('ejs');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+
+  let slideInfo = ['Ingeniería de Software', 'Algoritmos Evolutivos', 'Ciencia de Datos', 'Inteligencia Artificial'];
+
+  let files = fs.readdirSync(__dirname+'/public/images')
+
+  res.render('index', {
+    pageTitle: 'Home',
+    images: files,
+    slideInfo: slideInfo
+  })
+})
+
+app.get('/blog', (req, res) => {
+  res.render('blog', {
+    pageTitle: 'Mi blog'
+  }) 
+})
+
+app.get('/servicios', (req, res) => {
+  res.render('servicios', {
+    pageTitle: 'Mis servicios'
+  }) 
+})
+
+app.use((req, res, next) => {
+  res.status(404).render('404', {
+    pageTitle: 'Pagina no encontrada'
+  })
+}) 
+
+const leerArchivos = () => {
+  fs.readdir(__dirname+'/public/images', function(error, archivos){
+    if(error){
+      return console.log(error);
+    }
+    console.log("files",archivos)
+    return archivos
+  })
+}
+
+app.listen(PORT, () => {
+  console.log('Listen on port: ', PORT);
+})
+
